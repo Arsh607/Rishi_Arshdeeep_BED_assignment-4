c25247c3b4920851fb0c114e1b10e4bb0d8ce621
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-03-17 00:07:20 -0500

    feat: Set up the authorization middleware.

---
+++ src/api/v1/middleware/authorize.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { AuthorizationError } from "../errors/errors";
+
+interface AuthorizeOptions {
+    hasRole: string[];
+}
+
+const isAuthorized =
+    ({ hasRole }: AuthorizeOptions) =>
+    (req: Request, res: Response, next: NextFunction): void => {
+        const userRole = res.locals.role;
+
+        if (!userRole) {
+            next(
+                new AuthorizationError(
+                    "Forbidden: No role found",
+                    "ROLE_NOT_FOUND"
+                )
+            );
+            return;
+        }
+
+        if (!hasRole.includes(userRole)) {
+            next(
+                new AuthorizationError(
+                    "Forbidden: Insufficient role",
+                    "INSUFFICIENT_ROLE"
+                )
+            );
+            return;
+        }
+
+        next();
+    };
+
+export default isAuthorized;
